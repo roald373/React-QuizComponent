535efc64b6b1ae158c40b93e88ec501889abc3c9
Roald Haverkamp <roald11@hotmail.com>
2020-07-21 19:59:20 +0200

    add classname

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,9 +11,9 @@ class Quiz extends Component{
 
     render(){
         return <div>
-            <div class='QuizQuestion'>
-               {quizData.quiz_questions[0].instruction_text}
-            </div>
+                    <div className='QuizQuestion'>
+                        {quizData.quiz_questions[0].instruction_text}
+                    </div>
         </div>;
     }
 
